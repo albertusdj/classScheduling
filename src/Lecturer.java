395be0b6cec8ad092299e7a1fbeb0f44e7308c0e
Albertdj <albertusdjauharidjohan@gmail.com>
2018-11-21 00:07:40 +0700

    Modify Lecturer class

--- src/Lecturer.java
+++ src/Lecturer.java
@@ -1,6 +1,23 @@
-public class Lecturer {
-    // PIC : Albert
+import java.util.ArrayList;
 
-    // name
-    // time[]
+public class Lecturer {
+    private String name;
+    private ArrayList<TimeSlot> availability;
+
+    public Lecturer(String name) {
+        this.name = name;
+        availability = new ArrayList<TimeSlot>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<TimeSlot> getAvailability(){
+        return availability;
+    }
+
+    public void addTimeSlot(TimeSlot t) {
+        availability.add(t);
+    }
 }
